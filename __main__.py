e656879b27eb94ca36cc272a3bdf5cc7519987cb
Mark Dietzer <git@doridian.net>
2021-06-11 12:05:31 -0700

    huh

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from controller_em_message import EMMessageController
-from controller import LED_FAIL
+from controller import LED_FAIL, LED_LOCATE
 from controller_sgpio import SGPIOController
 
 from time import sleep
@@ -7,13 +7,13 @@ from time import sleep
 ctrl = SGPIOController('/sys/devices/pci0000:00/0000:00:11.0/0000:05:00.0/host7/bsg/sas_host7', 4)
 for i in range(0,4):
     print("Setting LED %d" % i)
-    ctrl.write(i, LED_FAIL)
+    ctrl.write(i, LED_LOCATE)
     sleep(1)
 ctrl.clear()
 
 ctrl = EMMessageController('/sys/devices/pci0000:00/0000:00:1f.2', 6)
 for i in range(0,4):
     print("Setting LED2 %d" % i)
-    ctrl.write(i, LED_FAIL)
+    ctrl.write(i, LED_LOCATE)
     sleep(1)
 ctrl.clear()
