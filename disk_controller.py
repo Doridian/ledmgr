0182a5b40a2782b0a2927d8b02f3306bf232ad28
Mark Dietzer <git@doridian.net>
2021-06-11 14:29:16 -0700

    test

--- disk_controller.py
+++ disk_controller.py
@@ -1,16 +1,37 @@
+from genericpath import exists
 from base_controller import BaseController
 
 class DiskController(BaseController):
     def __init__(self, config):
         super().__init__(config)
+        if 'subdev_find' in config:
+            self.subdev_find = config['subdev_find'].replace('*', '%d' % self.dev_value)
+        else:
+            self.subdev_find = None
 
     def get_index(self, disk):
         if not disk.startswith(self.dev):
             return -9001
 
-        # TODO: This is kinda stupid...
-        for i in range(self.subdev_value, 100):
-            if disk.startswith('%s/%s/' % (self.dev, self.subdev.replace('#', '%d' % i))):
-                return (i - self.subdev_value)
+        subdev_find = self.subdev
+        if self.subdev_find:
+            subdev_find = self.subdev_find
 
-        return -9002
+        # TODO: This is kinda stupid...
+        found_devphy = None
+        found_i = -9002
+        for i in range(1, 100):
+            devphy = '%s/%s/' % (self.dev, subdev_find.replace('#', '%d' % i))
+            if disk.startswith(devphy):
+                found_devphy = devphy
+                found_i = i
+                break
+
+        if self.subdev_find:
+            found_i = -9003
+            for i in range(self.subdev_value, 100): 
+                devphysub = '%s/%s/' % (found_devphy, self.subdev.replace('#', '%d' % i))
+                if exists(devphysub):
+                    found_i = i
+
+        return found_i - self.subdev_value
